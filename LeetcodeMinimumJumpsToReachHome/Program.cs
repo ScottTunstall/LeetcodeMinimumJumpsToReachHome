5854e1baaf7d3aa01305861cdb4b997221b76f2d
Scott Tunstall <scott.tunstall@ntlworld.com>
2021-05-24 17:45:31 +0100

    OK, I've decided to use memoization (caching) as my approach. It looks promising, but will it work with more complex leaps??

---
+++ LeetcodeMinimumJumpsToReachHome/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace LeetcodeMinimumJumpsToReachHome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var sol = new Solution();
+
+            var forbidden = new int[] {1,6,2,14,5,17,4};
+            int a = 16;
+            int b = 9;
+            int x = 7;
+
+            sol.MinimumJumps(forbidden, a, b, x);
+
+        }
+    }
+}
---
+++ LeetcodeMinimumJumpsToReachHome/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodeMinimumJumpsToReachHome
+{
+    public class Solution
+    {
+        private Dictionary<int, string> _numberToJumpsMap;
+
+
+        // https://leetcode.com/problems/minimum-jumps-to-reach-home/
+        public int MinimumJumps(int[] forbidden, int a, int b, int x)
+        {
+            if (x % a == 0)
+                return x / a;
+
+            _numberToJumpsMap = new();
+
+            _numberToJumpsMap[a] = "F";
+
+            for (int i = a; i <= x+a; i++)
+            {
+                if (!forbidden.Contains(i))
+                    memoize(i, a, b);
+            }
+
+            return 0;
+        }
+
+
+
+        private void memoize(int i, int jumpForward, int jumpBackward)
+        {
+            if (!_numberToJumpsMap.ContainsKey(i))
+            {
+                if (_numberToJumpsMap.ContainsKey(i - jumpForward))
+                {
+                    _numberToJumpsMap[i] = _numberToJumpsMap[i-jumpForward] + 'F';
+                }
+            }
+
+            if (_numberToJumpsMap.ContainsKey(i) && !_numberToJumpsMap.ContainsKey(i - jumpBackward) && (!_numberToJumpsMap[i].EndsWith("B")))
+            {
+                _numberToJumpsMap[i-jumpBackward] = _numberToJumpsMap[i] + "B";
+            }
+        }
+    }
+}
